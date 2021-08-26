8736a486e41250cc068482c6b18035845e73d92d
Deepak-Sharma28 <dk1999sharma1999@gmail.com>
2021-08-26 16:03:04 +0530

    added index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
 import express, {Application} from 'express';
-
 const app:Application = express();
-
 const array:number[] = [1,2,3]
-
-
 const PORT:number = 5000;
 
 app.listen(5000,()=>console.log('server is running'))
